38c4dac7a12d575d1faafb70d769d951845b4355
daniel lambarri <dalambarri@gmail.com>
2024-02-11 21:15:39 -0300

    test5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,14 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-// import node from "@astrojs/node";
 import tailwind from "@astrojs/tailwind";
 
-// import auth from "auth-astro";
+import auth from "auth-astro";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()],
-  output: "server"
-  // adapter: node({
-  //   mode: "standalone"
-  // })
-  ,
+  integrations: [svelte(), tailwind(), auth()],
+  output: "server",
   adapter: vercel()
 });
